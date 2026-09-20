c0b7b1fb44bf1ecc5c864953cf1c7913a6061e0b
Hriday-A <hriday.agarwal20103@gmail.com>
2026-09-19 20:39:09 -0500

    updated templates and views

--- illinois/urls.py
+++ illinois/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('', include('students.urls')),  # ← add this line
+    path("admin/", admin.site.urls),
+    path("students/", include("students.urls")),
 ]
--- students/views.py
+++ students/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from students.models import Student
+from .models import Student
 
 def home(request):
    return HttpResponse("Hello from the students app!")
+
+def student_list(request):
+   students= Student.objects.all()
+   context = {
+      "students": students
+   }
+   return render(request,"students/student_list.html",context)
